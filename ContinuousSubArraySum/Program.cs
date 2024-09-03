065778eb5c53dc4144b481f79268efff7e93ae6c
Rhemerson.monteiro <rhemerson.monteiro@engegraph.com.br>
2024-09-03 17:24:15 -0300

    Adicionar arquivos de projeto.

---
+++ ContinuousSubArraySum/Program.cs
@@ -0,0 +1,17 @@
+﻿using ContinuousSubArraySum;
+
+int[] nums = { 23, 2, 4, 6, 7};
+int[] nums1 = { 23, 2, 4, 6, 7, 1 };
+int[] nums2 = { 23, 2, 3, 6, 7};
+int[] nums3 = { 23, 2, 6, 7 };
+int[] nums4 = { 23, 5, 2, 4, 7, 3 };
+int[] nums5 = { 0, 6, 2, 1, 4, 7, 1 };
+int[] nums6 = { 23, 6, 2 };
+Console.WriteLine($"Resultado: {SubArraySum.CheckSubarraySum(nums, 6)}");
+Console.WriteLine($"Resultado1: {SubArraySum.CheckSubarraySum(nums1, 6)}");
+Console.WriteLine($"Resultado2: {SubArraySum.CheckSubarraySum(nums2, 6)}");
+Console.WriteLine($"Resultado3: {SubArraySum.CheckSubarraySum(nums3, 23)}");
+Console.WriteLine($"Resultado4: {SubArraySum.CheckSubarraySum(nums4, 6)}");
+Console.WriteLine($"Resultado4: {SubArraySum.CheckSubarraySum(nums5, 7)}");
+Console.WriteLine($"Resultado4: {SubArraySum.CheckSubarraySum(nums6, 8)}");
+Console.ReadLine();
---
+++ ContinuousSubArraySum/SubArraySum.cs
@@ -0,0 +1,43 @@
+﻿namespace ContinuousSubArraySum
+{
+    public static class SubArraySum
+    {
+        public static bool CheckSubarraySum(int[] nums, int k)
+        {
+            if(nums.Length >= 2)
+            {
+                int modulo = nums.Sum() % k;                
+                if (modulo.Equals(0)){ return true; }
+                
+                if(nums.Length > 2)
+                {                    
+                    int i = 0;
+                    int sum = 0;
+                    while (i < nums.Length)
+                    {   
+                        if (nums[i] < k && sum + nums[i] <= k)
+                        {
+                            sum += nums[i];
+                            int aux = i + 1;
+                            while (aux < nums.Length) 
+                            {
+                                sum += nums[aux];
+                                aux++;
+                                if (sum == k && sum % k == 0) { return true; }   
+                                if(sum > k)
+                                {
+                                    sum = 0;
+                                    break;
+                                }
+                            }
+                        }
+                        
+                        i++;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
